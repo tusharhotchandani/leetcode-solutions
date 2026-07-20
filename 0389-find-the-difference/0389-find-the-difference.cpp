72f40e76893eb08f96b21f678de150195d4a73f4
Tushar Hotchandani <tusharhotchandani1006@gmail.com>
2026-07-21 00:12:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.83%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        int sum = 0;
+        for(char &ch : t){
+            sum += ch;
+        }
+        
+        for(char &ch : s){
+            sum -= ch ;
+        }
+        
+        return (char)sum ;
+    }
+};
