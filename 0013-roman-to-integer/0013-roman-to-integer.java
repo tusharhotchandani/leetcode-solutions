44893967fb54c542ce591b74ba6d6cfa3eaabbc7
Tushar Hotchandani <tusharhotchandani1006@gmail.com>
2026-08-08 16:37:03 +0530

    Time: 5 ms (32.92%), Space: 46.6 MB (49.65%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -12,34 +12,15 @@ class Solution {
         int ans=0;
         char[] arr = s.toCharArray();
 
-        for(int i=0; i<arr.length; i++){
-            if(i<arr.length-1){
-                if( (arr[i] == 'I' && (arr[i+1] == 'V' || arr[i+1] =='X')) ||
-                    (arr[i] == 'X' && (arr[i+1] == 'L' || arr[i+1] =='C')) ||
-                    (arr[i] == 'C' && (arr[i+1] == 'D' || arr[i+1] =='M'))   ){
-                        ans = ans + (roman.get(arr[i+1]) - roman.get(arr[i]) );
-                        i = i+1;
-                }
-                else{
-                    ans = ans + roman.get(arr[i]);
-                }
-            }
-            else if(i>=1){
-                if( (arr[i-1] == 'I' && (arr[i] == 'V' || arr[i] =='X')) ||
-                    (arr[i-1] == 'X' && (arr[i] == 'L' || arr[i] =='C')) ||
-                    (arr[i-1] == 'C' && (arr[i] == 'D' || arr[i] =='M'))   ){
-                        ans = ans + (roman.get(arr[i]) - roman.get(arr[i-1]) );
-                        i = i+1;
-                }
-                else{
-                    ans = ans + roman.get(arr[i]);
-                }
+        for(int i =0 ; i<arr.length -1 ; i++){
+            if( (roman.get(arr[i])) < roman.get(arr[i+1]) ){
+                ans = ans - roman.get(arr[i]);
             }
             else{
                 ans = ans + roman.get(arr[i]);
             }
         }
-
+        ans = ans + roman.get(arr[arr.length-1]);
         return ans;
-    }
+    }    
 }
